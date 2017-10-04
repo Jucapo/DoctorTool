e1451043ba7178bfd24ef3dab3537bcb3cec7d24
Jucapo <jucapo05@gmail.com>
2017-10-04 18:38:12 -0500

    correccion de imagenes

--- src/providers/formulas/formulas.ts
+++ src/providers/formulas/formulas.ts
@@ -15,11 +15,11 @@ export class FormulasProvider {
 
   loadData(){
     this.data = [
-      { id:0, nombre: 'OSMP', descripcion: 'Osmolaridad Plasmatica', especialidad:'General', favorito: true, imagen:"https://image.ibb.co/ew0X8w/osmp.png" },
-      { id:1, nombre: 'IMC', descripcion: 'Indice Masa Corporal', especialidad:'General', favorito: false, imagen:"https://image.ibb.co/jZyKow/imc.png" }, 
-      { id:2, nombre: 'ASC', descripcion: 'Area Superficie Corporal', especialidad:'General', favorito: false, imagen:"https://image.ibb.co/d7Ps8w/asc.png" },
-      { id:3, nombre: 'LEE', descripcion: 'Indice de pronostico de LEE', especialidad:'Anestesiologia', favorito: false, imagen:"https://image.ibb.co/bMvzow/lee.png" },
-      { id:4, nombre: 'VTS', descripcion: 'Volumen Total Sanguinio', especialidad:'General', favorito: true, imagen:"https://image.ibb.co/dZdxab/vst.png" },
+      { id:0, nombre: 'OSMP', descripcion: 'Osmolaridad Plasmatica', especialidad:'General', favorito: true, imagen:'assets/icon_formulas/osmp.png' },
+      { id:1, nombre: 'IMC', descripcion: 'Indice Masa Corporal', especialidad:'General', favorito: false, imagen:'assets/icon_formulas/imc.png' }, 
+      { id:2, nombre: 'ASC', descripcion: 'Area Superficie Corporal', especialidad:'General', favorito: false, imagen:'assets/icon_formulas/asc.png' },
+      { id:3, nombre: 'LEE', descripcion: 'Indice de pronostico de LEE', especialidad:'Anestesiologia', favorito: false, imagen:"assets/icon_formulas/lee.png" },
+      { id:4, nombre: 'VTS', descripcion: 'Volumen Total Sanguinio', especialidad:'General', favorito: true, imagen:"assets/icon_formulas/vst.png" },
       
     ];
   }
